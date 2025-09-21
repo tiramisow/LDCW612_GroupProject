00aad1cae6faaf37c606d75e74f2a67cd1054fcc
Teh Li Wei <cha040405@gmail.com>
2025-09-22 04:45:27 +0800

    Added main menu loop and an exit option

--- MoviesRecommendationSystem.cpp
+++ MoviesRecommendationSystem.cpp
@@ -52,76 +52,86 @@ int main()
     movies.push_back(Genre("Zootopia", "Comedy"));
 
     int choice;
-    cout << "=== Welcome to Netflix OOP Terminal Assistant ===" << endl;
-    cout << "Please choose an option:" << endl;
-    cout << "1. Get recommendations by genre" << endl;
-    cout << "2. Search a movie and find its genre" << endl;
-    cout << "Enter option (1 or 2): ";
-    cin >> choice;
-    cin.ignore();
 
-    if (choice == 1)
+    while (true)
     {
-        int genreChoice;
-        cout << "Select a genre:\n";
-        cout << "1. Action\n2. Romance\n3. Sci-Fi\n4. Comedy\n";
-        cout << "Enter option (1-4): ";
-        cin >> genreChoice;
+        cout << "=== Welcome to Netflix OOP Terminal Assistant ===" << endl;
+        cout << "Please choose an option:" << endl;
+        cout << "1. Get recommendations by genre" << endl;
+        cout << "2. Search a movie and find its genre" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter option (1 or 2): ";
+        cin >> choice;
+        cin.ignore();
 
-        string selectedGenre;
-        switch (genreChoice)
+        if (choice == 1)
         {
-        case 1:
-            selectedGenre = "Action";
-            break;
-        case 2:
-            selectedGenre = "Romance";
-            break;
-        case 3:
-            selectedGenre = "Sci-Fi";
-            break;
-        case 4:
-            selectedGenre = "Comedy";
-            break;
-        default:
-            cout << "Invalid genre choice." << endl;
-            return 0;
-        }
+            int genreChoice;
+            cout << "Select a genre:\n";
+            cout << "1. Action\n2. Romance\n3. Sci-Fi\n4. Comedy\n";
+            cout << "Enter option (1-4): ";
+            cin >> genreChoice;
 
-        cout << "\nRecommended " << selectedGenre << " movies:\n";
-        for (auto &m : movies)
-        {
-            if (m.getGenre() == selectedGenre)
+            string selectedGenre;
+            switch (genreChoice)
             {
-                m.display();
-            }
-        }
-    }
-    else if (choice == 2)
-    {
-        string searchTitle;
-        cout << "\nEnter the movie title: ";
-        getline(cin, searchTitle);
-
-        bool found = false;
-        for (auto &m : movies)
-        {
-            if (toLower(m.getTitle()) == toLower(searchTitle))
-            {
-                cout << "Movie found! " << endl;
-                m.display();
-                found = true;
+            case 1:
+                selectedGenre = "Action";
                 break;
+            case 2:
+                selectedGenre = "Romance";
+                break;
+            case 3:
+                selectedGenre = "Sci-Fi";
+                break;
+            case 4:
+                selectedGenre = "Comedy";
+                break;
+            default:
+                cout << "Invalid genre choice." << endl;
+                continue;
+            }
+
+            cout << "\nRecommended " << selectedGenre << " movies:\n";
+            for (auto &m : movies)
+            {
+                if (m.getGenre() == selectedGenre)
+                {
+                    m.display();
+                }
             }
         }
-        if (!found)
+        else if (choice == 2)
         {
-            cout << "Sorry, movie not found in database." << endl;
+            string searchTitle;
+            cout << "\nEnter the movie title: ";
+            getline(cin, searchTitle);
+
+            bool found = false;
+            for (auto &m : movies)
+            {
+                if (toLower(m.getTitle()) == toLower(searchTitle))
+                {
+                    cout << "Movie found! " << endl;
+                    m.display();
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                cout << "Sorry, movie not found in database." << endl;
+            }
+        }
+        else if (choice == 3)
+        {
+            cout << "Exiting Program. Goodbye!" << endl;
+            break; // exit loop
+        }
+        else
+        {
+            cout << "Invalid option. Only option 1 to 3" << endl;
         }
     }
-    else
-    {
-        cout << "Invalid option." << endl;
-    }
-        return 0;
+    return 0;
 }
